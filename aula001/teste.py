dd514a646d564743e093baaf2e03935f9ad79cfa
arthurbrazmonai <arthurbrazmonai17@gmail.com>
2024-04-12 21:21:58 -0300

    hello world

---
+++ aula001/teste.py
@@ -0,0 +1,16 @@
+# autor arthur braz monai
+# data 12/04/2024
+
+# BIBLIOTECA
+import os 
+
+# LIMPAR CONSOLE 
+os.system('cls')
+
+# codigos executaveis
+
+print ('-' * 79)
+print ('Hello World !!! ' * 5)
+print ('-' * 79)
+print ()
+
